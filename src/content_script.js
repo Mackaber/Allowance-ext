907d0d0fa74d2c44aace85b02c80a9ccd1dc8886
Mackaber <x@mackaber.me>
2024-02-01 00:30:05 -0600

    Small cleanup you can now edit the message

--- src/content_script.js
+++ src/content_script.js
@@ -1,19 +1,19 @@
-console.log("Executing in: ", window.location.href);  
-
-let current_time
-
 // Create the new node you want to insert
 let block_node = document.createElement('div');
+let shadow_node = block_node.attachShadow({ mode: 'open' });
 
+const get_title = async () => {
+  const res = await chrome.storage.sync.get(['title']);
+  return res ? res.title : 'YOU CAN EDIT THIS MESSAGE, JUST CLICK IT';
+}
 
-const AddOverlay = () => {
-  block_node.innerHTML = `
+const AddOverlay = async () => {
+  shadow_node.innerHTML = `
   <div id="allowance-extension-overlay"
     style="position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 9000001; background-image: -webkit-linear-gradient(bottom, rgb(204, 204, 204) 0%, rgb(255, 255, 255) 75%); padding: 5em 1em 1em; text-align: center; color: rgb(0, 0, 0); font: 16px / 1 sans-serif;">
-    <img src="https://mackaber.me/testfiles/Pinkie_Pie_transparent.png" style="margin-bottom: 1em;width:200px;"><br />
-    <h1 style="margin: 0px 0px 0.5em;">Los hábitos no empiezan desde cero... </h1><br />
-    <select id="allowance-extension-duration">
-      <option value="" selected disabled>Selecciona un rango de tiempo</option>
+    <h1 id="allowance-extension-title" contenteditable="true" style="margin: 0px 0px 0.5em;border: none;outline: none;background: none;padding: 0;">${await get_title()}</h1><br />
+    <select id="allowance-extension-duration" style="font-size: 2em;">
+      <option value="" selected disabled>Select a time range...</option>
       <option value="60000">1 Minute</option>
       <option value="300000">5 Minutes</option>
       <option value="900000">15 Minutes</option>
@@ -23,22 +23,21 @@ const AddOverlay = () => {
   `;
   document.body.appendChild(block_node);
 
-  block_node
+  shadow_node
     .querySelector("#allowance-extension-duration")
     .addEventListener("change", (event) => {
-      document.getElementById("allowance-extension-overlay").remove();
-      //current_time = new Date();
-      console.log("Overlay closed at: ", current_time);
-
-      // 10 seconds interval
+      shadow_node.getElementById("allowance-extension-overlay").remove();
       setTimeout(() => {
-        console.log("Adding overlay at: ", new Date());
         AddOverlay();
       }, Number(event.target.value));
+    });
+  
+  shadow_node
+    .querySelector("#allowance-extension-title")
+    .addEventListener("input", (event) => {
+      console.log(event.target.innerHTML)
+      chrome.storage.sync.set({ title: event.target.innerHTML });
     });
 }
 
 AddOverlay();
-
-// Add click event listener to the new node
-
