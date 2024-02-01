a48f0c0aeefd818f4262fb040df41330c3ee6bea
Mackaber <x@mackaber.me>
2024-02-01 01:37:40 -0600

    WIP

--- src/background.js
+++ src/background.js
@@ -1,6 +1,30 @@
-function urlMatchesExcludedSites(url, excludedSites) {
-  return excludedSites.some(pattern => new RegExp(pattern).test(url));
+const urlMatchesExcludedSites = (url, excludedSites)  =>
+  excludedSites.some(pattern => new RegExp(pattern).test(url));
+
+const isSiteBlocked = async (url) => {
+  const res = await chrome.storage.sync.get(['excludedSites']);
+  return res ? urlMatchesExcludedSites(url, res.excludedSites) : false;
 }
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "toggle-site-blocked",
+    title: "Add this site to the blocked list",
+    contexts: ["action"] // Use "browser_action" for Manifest V2
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+
+  const blocked = await isSiteBlocked(tab.url)
+  const title = blocked ? "Remove this site from the blocked list" : "Add this site to the blocked list";
+  chrome.contextMenus.update("toggle-site-blocked", { title: title });
+
+  if (info.menuItemId === "toggle-site-blocked") {
+    // Your code to execute when the custom context menu entry is clicked
+    console.log("Tab: ", tab.url);
+  }
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.storage.sync.get(['excludedSites'], function(result) {
