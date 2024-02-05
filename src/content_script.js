fab008c551a3706f229539b5f5c6d4716c98141a
Mackaber <x@mackaber.me>
2024-02-05 01:46:30 -0600

    reformat code to avoid further bugs

--- src/content_script.js
+++ src/content_script.js
@@ -1,19 +1,28 @@
 const SHADOW_ROOT_ID = 'allowance-shadow-root-c89df1c2-8925-4902-b27d-3d44552907e0';
-// Create the new node you want to insert
-
-let block_node = document.createElement('div');
-block_node.id = SHADOW_ROOT_ID;
-let shadow_node = block_node.attachShadow({ mode: 'open' });
 
 const get_title = async () => {
   const res = await chrome.storage.sync.get(['title']);
   return res.title ? res.title : 'YOU CAN EDIT THIS MESSAGE, JUST CLICK IT';
 }
 
-const AddOverlay = async () => {
+const AttachShadowRoot = () => {
+  console.log('AttachShadowRoot');
+  const block_node = document.getElementById(SHADOW_ROOT_ID);
+  const shadow_node = block_node.attachShadow({ mode: 'open' });
+  createOverlay(shadow_node);
+}
+
+const DettachShadowRoot = () => {
+  console.log('DettachShadowRoot');
+  const block_node = document.getElementById(SHADOW_ROOT_ID);
+  const clone = block_node.cloneNode(); // Clone the element without the shadow DOM
+  clone.id = SHADOW_ROOT_ID;
+  block_node.parentNode.replaceChild(clone, block_node)
+}
+
+const createOverlay = async (shadow_node) => {
   shadow_node.innerHTML = `
-  <div id="allowance-extension-overlay"
-    style="position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 9000001; background-image: -webkit-linear-gradient(bottom, rgb(204, 204, 204) 0%, rgb(255, 255, 255) 75%); padding: 5em 1em 1em; text-align: center; color: rgb(0, 0, 0); font: 16px / 1 sans-serif;">
+  <div id="allowance-extension-overlay" style="position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 9000001; background-image: -webkit-linear-gradient(bottom, rgb(204, 204, 204) 0%, rgb(255, 255, 255) 75%); padding: 5em 1em 1em; text-align: center; color: rgb(0, 0, 0); font: 16px / 1 sans-serif;">
     <h1 id="allowance-extension-title" contenteditable="true" style="margin: 0px 0px 0.5em;border: none;outline: none;background: none;padding: 0;">${await get_title()}</h1><br />
     <select id="allowance-extension-duration" style="font-size: 2em;">
       <option value="" selected disabled>Select a time range...</option>
@@ -24,25 +33,27 @@ const AddOverlay = async () => {
     </select>
   </div>
   `;
-  document.body.appendChild(block_node);
 
   shadow_node
-    .querySelector("#allowance-extension-duration")
-    .addEventListener("change", (event) => {
-      shadow_node.getElementById("allowance-extension-overlay").remove();
-      setTimeout(() => {
-        AddOverlay();
-      }, Number(event.target.value));
-    });
-  
+  .querySelector("#allowance-extension-duration")
+  .addEventListener("change", (event) => {
+    DettachShadowRoot();
+    setTimeout(() => {
+      AttachShadowRoot();
+    }, Number(event.target.value));
+  });
+
   shadow_node
-    .querySelector("#allowance-extension-title")
-    .addEventListener("input", (event) => {
-      console.log(event.target.innerHTML)
-      chrome.storage.sync.set({ title: event.target.innerHTML });
-    });
+  .querySelector("#allowance-extension-title")
+  .addEventListener("input", (event) => {
+    console.log(event.target.innerHTML)
+    chrome.storage.sync.set({ title: event.target.innerHTML });
+  });
 }
 
-if(!document.getElementById(SHADOW_ROOT_ID)) {
-  AddOverlay();
-}
+// Create the new node you want to insert
+let block_node = document.createElement('div');
+block_node.id = SHADOW_ROOT_ID;
+if (!document.getElementById(SHADOW_ROOT_ID))
+  document.body.appendChild(block_node); 
+AttachShadowRoot();
